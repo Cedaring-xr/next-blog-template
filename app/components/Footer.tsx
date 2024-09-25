4614c9e7cc4134d50197e00f122b9a21a647bafa
mattraydev <cedaring.xr@gmail.com>
2024-09-25 08:59:30 -0600

    non-fixed footer

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -4,7 +4,7 @@ import Link from 'next/dist/client/link'
 
 const Footer = () => {
 	return (
-		<div className="w-full h-[30px] flex flex-row justify-between items-center bg-stone-900 bottom-0 fixed px-4 md:px-12 text-xs md:text-base">
+		<div className="w-full h-[30px] flex flex-row justify-between items-center bg-stone-900 bottom-0 px-4 md:px-12 text-xs md:text-base">
 			<span className="text-white">created by: Matt Ray</span>
 			<span className="text-white flex flex-row text-2xl gap-4">
 				<Link href="https://github.com/Cedaring-xr" target="_blank">
